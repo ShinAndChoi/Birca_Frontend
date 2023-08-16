ca8cece13bd8e9a684dfaa5ea2137ea3cd4cab5f
ShinKing <ssk7750@naver.com>
2023-08-16 15:08:25 +0900

    Feat. bottomNavi

---
+++ app/src/main/java/com/example/birca/fragment/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.birca.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.birca.R
+
+
+
+class HomeFragment : Fragment() {
+
+
+    //인스턴스 선언
+//    fun newInstance() : HomeFragment {
+//        return HomeFragment()
+//    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+}
