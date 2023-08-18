4e188ca732bf759074ad5cb17fa6cd8b8fd5b002
ShinKing <ssk7750@naver.com>
2023-08-18 16:59:44 +0900

    Feat. sharedPreference 추가

---
+++ app/src/main/java/com/example/birca/sharedPreference/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.example.birca.sharedPreference
+
+import android.app.Application
+
+class MyApplication : Application() {
+    companion object{
+        lateinit var preferences: PreferenceUtil
+    }
+
+    override fun onCreate() {
+        preferences = PreferenceUtil(applicationContext)
+        super.onCreate()
+    }
+}
