e4ac9779ebba5f7b6815b28c20d601852a1aecf1
ShinKing <ssk7750@naver.com>
2023-08-22 17:14:12 +0900

    Feat. 검색 화면 제작

--- app/src/main/java/com/example/birca/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/birca/fragment/SearchFragment.kt
@@ -1,10 +1,12 @@
 package com.example.birca.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import com.example.birca.R
 import com.example.birca.databinding.FragmentSearchBinding
 
@@ -34,8 +36,47 @@ class SearchFragment : Fragment() {
         val view = binding.root
 
 
+        //장소 선택
+        binding.selectCafeLocation.setOnClickListener {
+
+            val popupBase = binding.selectCafeLocation
+
+            var pop = PopupMenu(context,popupBase)
+
+            pop.menuInflater?.inflate(R.menu.cafe_loaction_list,pop.menu)
+
+            pop.show()
+
+            pop.setOnMenuItemClickListener { item->
+                when(item.itemId){
+                    R.id.hongdae_sinchon ->
+                        binding.selectCafeLocation.setText("홍대/신촌")
+                    R.id.seongsu_gundae ->
+                        binding.selectCafeLocation.setText("성수/건대")
+                    R.id.gangnam ->
+                        binding.selectCafeLocation.setText("강남")
+                }
+                false
+            }
+        }
+
+        //날짜 선택
+
+        binding.selectCafeDateCalendar.setOnDateChangeListener{ view, year, month, dayOfMonth ->
+
+            binding.selectCafeDate.setText("${year}년 ${month+1}월 ${dayOfMonth}일")
+            Log.d("selecteddate", "${year}년 ${month+1}월 ${dayOfMonth}일")
+        }
+
+        //검색하기 버튼
+        binding.btnSearch.setOnClickListener {
+
+        }
+
         return view
     }
+
+
 
 
 }
