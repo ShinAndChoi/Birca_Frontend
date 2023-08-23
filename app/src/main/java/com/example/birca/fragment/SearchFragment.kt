a0bce713c41d994b7b678736a1c79f71b3acba96
ShinKing <ssk7750@naver.com>
2023-08-23 19:06:51 +0900

    Feat. 카페 검색 및 대관 수정

--- app/src/main/java/com/example/birca/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/birca/fragment/SearchFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import android.widget.Toast
 import com.example.birca.R
 import com.example.birca.databinding.FragmentSearchBinding
 
@@ -15,6 +16,10 @@ class SearchFragment : Fragment() {
 
     private var _binding : FragmentSearchBinding? = null
     private val binding get() = _binding!!
+
+    var idol = ""
+    var cafe_location = ""
+    var cafe_date = ""
 
     //인스턴스 선언
 //    fun newInstance() : SearchFragment {
@@ -58,6 +63,8 @@ class SearchFragment : Fragment() {
                 }
                 false
             }
+
+
         }
 
         //날짜 선택
@@ -65,11 +72,25 @@ class SearchFragment : Fragment() {
         binding.selectCafeDateCalendar.setOnDateChangeListener{ view, year, month, dayOfMonth ->
 
             binding.selectCafeDate.setText("${year}년 ${month+1}월 ${dayOfMonth}일")
+            cafe_date = binding.selectCafeDate.text.toString()
             Log.d("selecteddate", "${year}년 ${month+1}월 ${dayOfMonth}일")
         }
 
-        //검색하기 버튼
+        //검색 버튼
         binding.btnSearch.setOnClickListener {
+
+            idol = binding.textIdol.text.toString()
+            cafe_location = binding.selectCafeLocation.text.toString()
+            cafe_date = binding.selectCafeDate.text.toString()
+
+            Log.d("click", "click")
+          if(idol==""){
+              Toast.makeText(context, "아이돌을 입력해주세요", Toast.LENGTH_SHORT).show()
+          } else {
+              //다음 페이지
+              Toast.makeText(context, "다음 페이지", Toast.LENGTH_SHORT).show()
+          }
+
 
         }
 
