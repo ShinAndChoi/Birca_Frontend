605ab04821c3518efe789626269e12cb6d9c9cb0
ShinKing <ssk7750@naver.com>
2023-09-01 16:38:29 +0900

    Feat. 갤러리 권한 수정 중

--- app/src/main/java/com/example/birca/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/birca/fragment/RegisterFragment.kt
@@ -1,6 +1,10 @@
 package com.example.birca.fragment
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +12,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+
 import com.example.birca.R
 import com.example.birca.databinding.FragmentRegisterBinding
 import com.example.birca.model.cafeRegisterInfoBody
@@ -15,14 +23,19 @@ import com.example.birca.model.cafeRegisterInfoBody
 
 class RegisterFragment : Fragment() {
 
+    //권한 요청 코드
+    private val REQUEST_PERMISSION_CODE = 123
+
+    private var PICK_IMAGE_REQUEST = 1
+
     private var _binding : FragmentRegisterBinding? = null
     private val binding get() = _binding!!
 
     var cafe_name = ""
     var cafe_location = ""
     var cafe_introduction = ""
-    var cafe_image = ""
-    var cafe_businessLicense = ""
+    var cafe_image_URI = ""
+    var cafe_businessLicense_URI = ""
     var cafe_contact =""
 
 //    var registerInfo = cafeRegisterInfoBody(cafe_name,cafe_introduction,cafe_location,cafe_contact)
@@ -42,6 +55,7 @@ class RegisterFragment : Fragment() {
     ): View? {
         _binding = FragmentRegisterBinding.inflate(inflater,container,false)
         val view = binding.root
+
 
 
 
@@ -72,6 +86,35 @@ class RegisterFragment : Fragment() {
             Log.d("cafe_location", cafe_location)
         }
 
+        //카페 사진 클릭
+        binding.btnCafeImage.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermission()
+            } else {
+                // 이미 권한이 부여된 경우 갤러리 열기 또는 다른 작업 수행
+                PICK_IMAGE_REQUEST = 2
+                openGallery()
+            }
+        }
+
+        //사업자등록증 클릭
+        binding.btnCafeImage.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermission()
+            } else {
+                // 이미 권한이 부여된 경우 갤러리 열기 또는 다른 작업 수행
+                PICK_IMAGE_REQUEST = 3
+                openGallery()
+            }
+        }
 
         //등록 버튼
         binding.btnRegister.setOnClickListener {
@@ -95,11 +138,72 @@ class RegisterFragment : Fragment() {
             } else {
                 //통신하기
 
+
                 //body에 담기
                 var registerInfo = cafeRegisterInfoBody(cafe_name,cafe_introduction,cafe_location,cafe_contact)
             }
         }
         return view
+    }
+
+
+    // 갤러리를 여는 함수
+    private fun openGallery() {
+        // 갤러리를 열고 이미지를 선택하는 코드를 여기에 추가합니다.
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            val selectedImageUri = data.data
+            // 이제 selectedImageUri를 사용하여 선택한 이미지의 URI를 처리할 수 있습니다.
+            if(PICK_IMAGE_REQUEST == 2) {
+                //카페 이미지 클릭시
+                //glide로 imageview 적용
+                Glide.with(requireContext())
+                    .load(selectedImageUri)
+                    .into(binding.btnCafeImage)
+            } else if ( PICK_IMAGE_REQUEST ==3) {
+                //사업자 등록증 클릭시
+                //glide로 imageview 적용
+                Glide.with(requireContext())
+                    .load(selectedImageUri)
+                    .into(binding.btnBusinessLicense)
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            // 권한 요청
+            requestPermissions(
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_PERMISSION_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 부여된 경우 갤러리 열기 또는 다른 작업 수행
+                openGallery()
+            } else {
+                // 권한이 거부된 경우 사용자에게 알림을 표시하거나 다른 조치를 취할 수 있습니다.
+            }
+        }
     }
 
 
