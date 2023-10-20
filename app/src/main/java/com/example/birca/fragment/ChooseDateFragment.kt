e227a7c6144d476d8d12e63bd580c290a67e5942
cityshin <ssk7750@naver.com>
2023-10-20 16:34:27 +0900

    Feat. bottom navigation visibility 추가

--- app/src/main/java/com/example/birca/fragment/ChooseDateFragment.kt
+++ app/src/main/java/com/example/birca/fragment/ChooseDateFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.birca.MainActivity
 import com.example.birca.R
 import com.example.birca.base.BaseFragment
 import com.example.birca.databinding.FragmentChooseDateBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class ChooseDateFragment : BaseFragment<FragmentChooseDateBinding>(R.layout.fragment_choose_date) {
@@ -15,6 +17,11 @@ class ChooseDateFragment : BaseFragment<FragmentChooseDateBinding>(R.layout.frag
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val mainActivity = activity as MainActivity
+
+        val bottomNav = mainActivity.findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        bottomNav.visibility = View.GONE
     }
 
     override fun onCreateView(
