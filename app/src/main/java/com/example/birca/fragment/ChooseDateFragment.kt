c34f1be99789f90b634f2f782fd3521bed3a07ee
cityshin <ssk7750@naver.com>
2023-10-20 18:04:39 +0900

    Feat. 클릭 이벤트 생성

--- app/src/main/java/com/example/birca/fragment/ChooseDateFragment.kt
+++ app/src/main/java/com/example/birca/fragment/ChooseDateFragment.kt
@@ -1,10 +1,13 @@
 package com.example.birca.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.birca.FragmentActivity
 import com.example.birca.MainActivity
 import com.example.birca.R
 import com.example.birca.base.BaseFragment
@@ -17,9 +20,9 @@ class ChooseDateFragment : BaseFragment<FragmentChooseDateBinding>(R.layout.frag
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val mainActivity = activity as MainActivity
+        val fragmentActivity = activity as FragmentActivity
 
-        val bottomNav = mainActivity.findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val bottomNav = fragmentActivity.findViewById<BottomNavigationView>(R.id.bottom_nav)
 
         bottomNav.visibility = View.GONE
     }
@@ -35,6 +38,21 @@ class ChooseDateFragment : BaseFragment<FragmentChooseDateBinding>(R.layout.frag
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnBack.setOnClickListener {
+            val reservedCafeListFragment = ReservedCafeListFragment()
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.frameArea, reservedCafeListFragment)
+//                addToBackStack(null)
+                commit()
+            }
+        }
+
+        
+        binding.calendar.setOnDateChangeListener { calendarView, year, month, dayOfMonth ->
+
+            binding.btnSelectDateColor.setBackgroundColor(Color.parseColor("#A1B4FF"))
+            Toast.makeText(context,"${year}년 ${month}월 ${dayOfMonth}일",Toast.LENGTH_SHORT).show()
+        }
 
     }
 
