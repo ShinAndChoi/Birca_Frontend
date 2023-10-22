89f0c584c9f784f41c4da27655fe0034d548cf6c
cityshin <ssk7750@naver.com>
2023-10-22 18:00:08 +0900

    Feat. activity 추가

---
+++ app/src/main/java/com/example/birca/FragmentCafeOwnerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.birca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FragmentCafeOwnerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_cafe_owner)
+    }
+}
